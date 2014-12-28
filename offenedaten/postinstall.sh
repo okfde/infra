792addf0431de533ce32fdcdc69e86c66f81897a
Matt Fullerton <matt.fullerton@gmail.com>
2014-12-28 02:37:29 +0100

    Change CKAN DB password

--- offenedaten/postinstall.sh
+++ offenedaten/postinstall.sh
@@ -1,3 +1,6 @@
+#Change password
+docker run -i -t --link db:db --link offenedaten_de bin/bash -c 'psql -U ckan -h 172.17.0.11 -d ckan -c \\password'
+
 #Initialize harvesting
 docker run -i -t --link db:db --link solr:solr offenedaten_de sbin/my_init -- bin/bash -c '$CKAN_HOME/bin/paster --plugin=ckanext-harvest harvester initdb --config=$CKAN_CONFIG/ckan.ini'
 
